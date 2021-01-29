cfe9a8475f89b3e78d3a3c410b6eb7b708658aa6
wangzl1163 <wangzl1163@qq.com>
2021-01-29 14:22:30 +0800

    修复配置bug

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -88,15 +88,20 @@ module.exports = appInfo => {
 
 
    // 项目特别配置
-   config.devops = {
+   config.appSelf = {
       version: process.env.VERSION || 'VERSION' + new Date().getTime(),
       deployTime: moment(new Date().getTime()).format('YYYY-MM-DD')
    }
 
    // 设置集群启动配置。
+   // 参考：https://eggjs.org/zh-cn/core/cluster-and-ipc.html#mobileAside
+   // 注意：egg-scripts在没有配置works参数时会默认启动与服务器CPU核数相匹配的进程数，此时不能配置cluster模块
+   //       否则，无法启动。如果配置了cluster模块，要设置start命令的works为1，示例：
+   //       eggctl start --title=egg-server-http-proxy --works=1
+   //       另外，配置cluster模块时指定了端口，在不必在npm命令中设置端口，
    config.cluster = {
       listen: {
-         port: 8001,
+         port: 9001,
          hostname: '0.0.0.0',
       }
    }

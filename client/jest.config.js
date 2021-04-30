2b405a46a37c2a1df6195e313675e8087d934580
wangzl1163 <wangzl1163@qq.com>
2021-04-30 17:40:03 +0800

    配置单元测试

--- client/jest.config.js
+++ client/jest.config.js
@@ -76,8 +76,7 @@ module.exports = {
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/test/unit/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/unit/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/test/unit/__mocks__/styleMock.js',
     '^@views/(.*)$': '<rootDir>/src/views/$1',
     '^@/(.*)$': '<rootDir>/src/$1'

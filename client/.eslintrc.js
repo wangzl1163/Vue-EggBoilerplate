18ca3959bec09276d435784cacc40ef1fa301483
wangzl1163 <wangzl1163@qq.com>
2021-05-06 13:55:52 +0800

    修正eslint配置与其插件配置

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,8 +1,18 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  extends: ['plugin:vue/recommended', 'standard', 'prettier', 'prettier/standard'],
-  plugins: ['vue', 'prettier', 'standard'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'standard',
+    'prettier',
+    'prettier/standard',
+    'plugin:import/errors', // 配置eslint-plugin-import
+    'plugin:import/warnings', // 配置eslint-plugin-import
+    'plugin:node/recommended', // 配置eslint-plugin-node
+    'plugin:promise/recommended' // 配置eslint-plugin-promise
+  ],
+  plugins: ['prettier', 'standard', 'promise', '@babel'],
   rules: {
     'prettier/prettier': ['error', {"tabWidth": 3}],
     'vue/no-async-in-computed-properties': 'off',
@@ -20,8 +30,10 @@ module.exports = {
     browser: true,
     node: true
   },
+  parser: "vue-eslint-parser", // eslint-plugin-vue需要此配置，参考：https://eslint.vuejs.org/user-guide/#usage
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser', // 配置使用@babel/eslint-parser，参考：https://eslint.vuejs.org/user-guide/#usage，其中的babel-eslint已经改为babel内置的@babel/eslint-parser
+    ecmaVersion: 2020,
     clearSelection: {
       jsx: true
     }

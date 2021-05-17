41a9e5a75f70918774dc7b6dfd11aaadc54cf75b
wangzl1163 <wangzl1163@qq.com>
2021-05-17 14:30:49 +0800

    解决babel配置文件不在根目录中报找不到的问题

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,19 +1,20 @@
 // https://eslint.org/docs/user-guide/configuring
 
+const path = require('path')
+
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:vue/recommended',
     'standard',
-    'prettier',
+    'plugin:vue/recommended',
     'plugin:import/errors', // 配置eslint-plugin-import
     'plugin:import/warnings', // 配置eslint-plugin-import
     'plugin:node/recommended', // 配置eslint-plugin-node
-    'plugin:promise/recommended' // 配置eslint-plugin-promise
+    'plugin:promise/recommended', // 配置eslint-plugin-promise
+    'prettier',
   ],
   plugins: ['standard', 'promise', '@babel'],
   rules: {
-    'prettier/prettier': ['error', {"tabWidth": 3}],
     'vue/no-async-in-computed-properties': 'off',
     'vue/max-attributes-per-line': 'off' , // 关闭Vue，使用prettier自动根据行宽判断
     'vue/html-self-closing':["error",{
@@ -32,9 +33,11 @@ module.exports = {
   parser: "vue-eslint-parser", // eslint-plugin-vue需要此配置，参考：https://eslint.vuejs.org/user-guide/#usage
   parserOptions: {
     parser: '@babel/eslint-parser', // 配置使用@babel/eslint-parser，参考：https://eslint.vuejs.org/user-guide/#usage，其中的babel-eslint已经改为babel内置的@babel/eslint-parser
-    ecmaVersion: 2020,
-    clearSelection: {
-      jsx: true
+    ecmaVersion: 2021,
+    babelOptions: {
+      // 指定babel配置文件路径，解决了找不到babel配置文件问题
+      // 参考：https://www.npmjs.com/package/@babel/eslint-parser的“Additional parser configuration”节
+      configFile: path.join(__dirname, "/babel.config.js")
     }
   },
   root: true

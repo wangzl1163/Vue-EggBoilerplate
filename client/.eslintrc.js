22bb0bf35fe2bc8bedfa7f34a010804c23863091
王占领 <wangzl1163@qq.com>
2021-05-18 21:00:31 +0800

    配置提交代码时自动检测代码规范并格式化

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,41 +1,48 @@
 // https://eslint.org/docs/user-guide/configuring
 
+const path = require('path')
+
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/recommended',
-    'standard',
-    'prettier',
-    'plugin:import/errors', // 配置eslint-plugin-import
-    'plugin:import/warnings', // 配置eslint-plugin-import
-    'plugin:node/recommended', // 配置eslint-plugin-node
-    'plugin:promise/recommended' // 配置eslint-plugin-promise
-  ],
-  plugins: ['standard', 'promise', '@babel'],
-  rules: {
-    'prettier/prettier': ['error', {"tabWidth": 3}],
-    'vue/no-async-in-computed-properties': 'off',
-    'vue/max-attributes-per-line': 'off' , // 关闭Vue，使用prettier自动根据行宽判断
-    'vue/html-self-closing':["error",{
-      'html':{
-        'void':'any'
+   root: true,
+   extends: [
+      'eslint:recommended',
+      'standard',
+      'plugin:vue/recommended',
+      'plugin:import/errors', // 配置eslint-plugin-import
+      'plugin:import/warnings', // 配置eslint-plugin-import
+      'plugin:node/recommended', // 配置eslint-plugin-node
+      'plugin:promise/recommended', // 配置eslint-plugin-promise
+      'prettier' // 需要放在最后，以便覆盖其他配置
+   ],
+   plugins: ['@babel'],
+   rules: {
+      'node/no-unsupported-features/es-syntax': 'off',
+      'vue/no-async-in-computed-properties': 'off',
+      'vue/max-attributes-per-line': 'off', // 关闭Vue，使用prettier自动根据行宽判断
+      'vue/html-self-closing': [
+         'error',
+         {
+            html: {
+               void: 'any'
+            }
+         }
+      ],
+      'vue/html-closing-bracket-newline': 'off',
+      'vue/html-indent': 'off',
+      'vue/singleline-html-element-content-newline': 'off'
+   },
+   env: {
+      browser: true,
+      node: true,
+      es6: true // 使eslint支持es6语法
+   },
+   parser: 'vue-eslint-parser', // eslint-plugin-vue需要此配置，参考：https://eslint.vuejs.org/user-guide/#usage
+   parserOptions: {
+      parser: '@babel/eslint-parser', // 配置使用@babel/eslint-parser，参考：https://eslint.vuejs.org/user-guide/#usage，其中的babel-eslint已经改为babel内置的@babel/eslint-parser
+      sourceType: "module",
+      ecmaVersion: 2020,
+      babelOptions: {
+         configFile: path.join(__dirname, '/babel.config.js')
       }
-    }],
-    'vue/html-closing-bracket-newline':'off',
-    'vue/html-indent':'off',
-    'vue/singleline-html-element-content-newline':'off'
-  },
-  env: {
-    browser: true,
-    node: true
-  },
-  parser: "vue-eslint-parser", // eslint-plugin-vue需要此配置，参考：https://eslint.vuejs.org/user-guide/#usage
-  parserOptions: {
-    parser: '@babel/eslint-parser', // 配置使用@babel/eslint-parser，参考：https://eslint.vuejs.org/user-guide/#usage，其中的babel-eslint已经改为babel内置的@babel/eslint-parser
-    ecmaVersion: 2020,
-    clearSelection: {
-      jsx: true
-    }
-  },
-  root: true
+   }
 }
--- client/src/Views/MySQL/Detail/index.vue
+++ client/src/Views/MySQL/Detail/index.vue
@@ -1,24 +1,20 @@
 <template>
-	<div>
-		详情
-	</div>
+   <div>详情</div>
 </template>
 
 <script>
 export default {
+
+	components: {},
 	data () {
 		return {
 		}
 	},
-
-	components: {},
 
 	computed: {},
 
 	methods: {},
 
 }
-
 </script>
-<style lang='less'>
-</style>
+<style lang="less"></style>

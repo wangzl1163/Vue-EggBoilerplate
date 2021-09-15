8a1e1adda8017ed2d8f063a11970227997729109
wangzl1163 <wangzl1163@qq.com>
2021-09-15 13:58:20 +0800

    优化server端项目配置

--- server/.autod.conf.js
+++ server/.autod.conf.js
@@ -1,21 +1,34 @@
 'use strict'
 
 module.exports = {
-  write: false,
+  write: true,
   prefix: '^',
   devprefix: '^',
   plugin: 'autod-egg',
   test: ['test'],
-  dep: ['egg', 'egg-scripts', 'egg-view-nunjucks'],
+  dep: [
+    'egg',
+    'egg-scripts',
+    'egg-view-nunjucks',
+    'egg-http-proxy',
+    'egg-history-api-fallback',
+    'egg-logrotator'
+  ],
   devdep: [
     'egg-ci',
     'egg-bin',
     'egg-mock',
+    'acorn',
     'autod',
     'autod-egg',
     'eslint',
     'eslint-config-egg',
+    'mocha',
+    'webpack',
+    'koa-webpack'
+  ],
+  keep: [
     'webpack'
   ],
-  exclude: ['./test', './app/public']
+  exclude: ['./test', './app/public', './siteFile']
 }
